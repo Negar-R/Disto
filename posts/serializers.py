f9140ce62f4464d999e5575c087609589f218452
Negar-R <negar.r.78.ioi@gmail.com>
2021-01-26 10:40:11 +0330

    delete apps

--- posts/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework_mongoengine.serializers import DocumentSerializer
-from posts.models import Post
-
-
-class PostSerializer(DocumentSerializer):
-    class Meta:
-        model = Post
-        fields = '__all__'
